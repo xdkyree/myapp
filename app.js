13b0578d393f85a585a34517f4d692e9c328e1d7
lukax200 <lukax200@gmail.com>
2022-01-08 00:06:36 +0100

    Added proper routing and installed websocket

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const http = require("http");
+const websocket = require("ws");
+
+const indexRouter = require("./routes/index");
+
+if(process.argv.length < 3) {
+    console.log("Error: expected a port as argument");
+    process.exit(1);
+}
 
 const port = process.argv[2];
 const app = express();
+
+app.get("play", indexRouter);
+app.get("/", indexRouter);
+
 
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get("/play", function(req, res) {
+  res.sendFile("game.html", {root: "./public"});
+})
+
+
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res,) {
+  console.log("eo");
+  res.sendFile("splash.html", {root: "./public"});
 });
 
 module.exports = router;
